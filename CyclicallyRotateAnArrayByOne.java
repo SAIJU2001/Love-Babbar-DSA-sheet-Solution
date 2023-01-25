83687b7247f65c761894a9bdd331a75ef69fc06c
saikat naskar <102968768+SAIJU2001@users.noreply.github.com>
2023-01-25 09:30:50 +0530

    Add files via upload

---
+++ CyclicallyRotateAnArrayByOne.java
@@ -0,0 +1,85 @@
+/************love babbar dsa sheet******
+
+            Cyclically rotate an array by one
+
+Given an array, rotate the array by one position in clock-wise direction.
+ 
+
+Example 1:
+Input:
+N = 5
+A[] = {1, 2, 3, 4, 5}
+Output:
+5 1 2 3 4
+ 
+Example 2:
+Input:
+N = 8
+A[] = {9, 8, 7, 6, 4, 2, 1, 3}
+Output:
+3 9 8 7 6 4 2 1
+ 
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function rotate() which takes the array A[] and its size N as inputs and modify the array in place.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1<=N<=105
+0<=a[i]<=105            */
+
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CyclicallyRotateAnArrayByOne 
+{
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) 
+                a[i] = Integer.parseInt(inputLine[i]);
+            
+            Compute obj = new Compute();
+            obj.rotate(a, n);
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+                output.append(a[i]+" ");
+            System.out.println(output);    
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Compute 
+{    
+    public void rotate(int arr[], int n)
+    {
+            int j=n-1;
+            int temp=arr[j];
+            while( j>0 )
+            {
+                arr[j]=arr[j-1];
+                j--;
+            }
+            arr[j]=temp;
+    }
+}
